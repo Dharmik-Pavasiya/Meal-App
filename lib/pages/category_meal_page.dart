613c662496f8a0249ce572fddf7d44c506c1897e
Dharmik-Pavasiya <dharmikpavasiya808@gmail.com>
2022-09-04 17:34:55 +0530

    Adding Meal Model And Data

--- lib/pages/category_meal_page.dart
+++ lib/pages/category_meal_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
 
 class CategoryMealsPage extends StatelessWidget {
   const CategoryMealsPage({Key? key}) : super(key: key);
@@ -8,18 +9,26 @@ class CategoryMealsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final routeArgs = ModalRoute
+        .of(context)!
+        .settings
+        .arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('$categoryTitle'),
       ),
-      body: const Center(
-        child: Text('The Recipes For The Category!'),
+      body: ListView.builder(
+        itemBuilder: (conatext, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
