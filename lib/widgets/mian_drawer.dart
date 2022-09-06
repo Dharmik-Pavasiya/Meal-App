adbfb8801f52f951f1afdd873480da55fc38df8b
Dharmik-Pavasiya <dharmikpavasiya808@gmail.com>
2022-09-06 19:15:20 +0530

    Replcaing Pages

--- lib/widgets/mian_drawer.dart
+++ lib/widgets/mian_drawer.dart
@@ -15,7 +15,7 @@ class MainDrawer extends StatelessWidget {
         ),
       ),
       onTap: (){
-        Navigator.of(context).pushNamed(routeName);
+        Navigator.of(context).pushReplacementNamed(routeName);
       },
     );
   }
